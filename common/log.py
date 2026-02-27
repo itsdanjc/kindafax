628c694034abb6900e183783de62cc6d7bfa6eae
itsdanjc <dan@itsdanjc.com>
2026-02-27 17:12:57 +0000

    Fix: Rotate log files and remove ansi

--- common/log.py
+++ common/log.py
@@ -1,36 +1,53 @@
 import logging
 import sys
 from typing import Final, Optional, Union, TypeAlias
+from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
 
 LOGGER_NAME_PREFIX: Final[str] = "kindafax"
 LOGGER_NAME_SEPARATOR: Final[str] = "."
 LOGGER_DATE_FORMAT: Final[str] = "%Y-%m-%d %H:%M:%S"
-LOGGER_FILE_NAME: Final[str] = "kindafax.log"
+LOGGER_FILE_NAME: Final[str] = f"kindafax.log"
 
-LogHandler: TypeAlias = Union[logging.StreamHandler, logging.FileHandler]
+LogHandler: TypeAlias = Union[logging.StreamHandler, TimedRotatingFileHandler]
 
-FORMAT: Final[dict[int, str]] = {
+FORMAT_COLORED: Final[dict[int, str]] = {
     logging.DEBUG:
         "\033[36m%(asctime)s\033[0m  %(message)s",  # Cyan Timestamp. Uncolored Message.
     logging.INFO:
         "\033[36m%(asctime)s\033[0m  %(message)s",  # Same as debug
     logging.WARNING:
-        "\033[36m%(asctime)s\033[33m %(levelname)s: %(message)s\033[0m",  # Yellow Message
+        "\033[36m%(asctime)s\033[33m  %(levelname)s: %(message)s\033[0m",  # Yellow Message
     logging.ERROR:
-        "\033[36m%(asctime)s\033[31m %(levelname)s: %(message)s\033[0m",  # Red Message
+        "\033[36m%(asctime)s\033[31m  %(levelname)s: %(message)s\033[0m",  # Red Message
     logging.CRITICAL:
-        "\033[36m%(asctime)s\033[31m %(levelname)s: %(message)s\033[0m",  # Same as error
+        "\033[36m%(asctime)s\033[31m  %(levelname)s: %(message)s\033[0m",  # Same as error
 }
 
+FORMAT: Final[dict[int, str]] = {
+    logging.DEBUG:      "%(asctime)s  %(message)s",
+    logging.INFO:       "%(asctime)s  %(message)s",
+    logging.WARNING:    "%(asctime)s  %(levelname)s: %(message)s",
+    logging.ERROR:      "%(asctime)s  %(levelname)s: %(message)s",
+    logging.CRITICAL:   "%(asctime)s  %(levelname)s: %(message)s",
+}
 
 class LoggerFormat(logging.Formatter):
-    def __init__(self) -> None:
+    def __init__(self, use_ansi: bool) -> None:
         super().__init__(datefmt=LOGGER_DATE_FORMAT)
-        self._formatters = {
-            level: logging.Formatter(fmt, LOGGER_DATE_FORMAT)
-            for level, fmt in FORMAT.items()
-        }
+        self._formatters: dict[int, logging.Formatter]
+
+        if use_ansi:
+            self._formatters = {
+                level: logging.Formatter(fmt, LOGGER_DATE_FORMAT)
+                for level, fmt in FORMAT_COLORED.items()
+            }
+
+        else:
+            self._formatters = {
+                level: logging.Formatter(fmt, LOGGER_DATE_FORMAT)
+                for level, fmt in FORMAT.items()
+            }
 
     def format(self, record: logging.LogRecord) -> str:
         formatter = self._formatters.get(
@@ -55,17 +72,30 @@ def get_handlers(out: Optional[Path] = None) -> LogHandler:
     Create a logger handler. Also define a formatter for messages.
 
     :param out: Optional path to log file. Otherwise, log to `stdout`.
-    :return: `FileHandler` if logging to file, else `StreamHandler`
+    :return: `RotatingFileHandler` if logging to file, else `StreamHandler`
     """
     handler: LogHandler
+    use_ansi: bool
 
     if out:
-        out = out.joinpath(LOGGER_FILE_NAME)
-        handler = logging.FileHandler(out)
+        use_ansi = False
+        handler = TimedRotatingFileHandler(
+            filename = out.joinpath(LOGGER_FILE_NAME),
+            when = "midnight",
+            backupCount = 5,
+            encoding = "utf-8",
+            utc = False,
+        )
+        handler.namer = lambda n: "{}{}".format(
+            n.replace(".log", ""), ".log"
+        )
     else:
+        use_ansi = True
         handler = logging.StreamHandler(sys.stdout)
 
-    handler.setFormatter(LoggerFormat())
+    handler.setFormatter(
+        LoggerFormat(use_ansi)
+    )
     return handler
 
 
