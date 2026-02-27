629eeb16256a9ffed1e1afc6c237823bceca9d62
itsdanjc <dan@itsdanjc.com>
2026-02-27 13:27:05 +0000

    Fix: Directory structure for config

--- common/config.py
+++ common/config.py
@@ -3,26 +3,38 @@ from pathlib import Path
 from typing import Final, Any, Optional, Type
 import tomllib
 
+CONFIG_FILE: Final[str] = "config.toml"
+APP_NAME: Final[str] = "kindafax"
+APP_AUTHOR: Final[str] = "KindaFax"
+
 class Config:
     """
     Represents a config file.
 
-    This object should be created for each module that uses it.
+    This object should be instantiated per module.
 
     The configuration file is stored in the following locations:
-        - **On Windows:**     `C:/ProgramData/kindafax/kindafax-<module>/config.toml`
-        - **On Linux:**       `/etc/xdg/kindafax-<module>/config.toml`
-        - **On MacOS:**       `/Library/Application Support/kindafax-<module>/config.toml`
-        - **On Android**      `/data/data/<pkg>/shared_prefs/kindafax-<module>/config.toml`
+        - **On Windows:**     `C:/ProgramData/KindaFax/kindafax/<module>/config.toml`
+        - **On Linux:**       `/etc/xdg/kindafax/<module>/config.toml`
+        - **On MacOS:**       `/Library/Application Support/kindafax/<module>/config.toml`
+        - **On Android**      `/data/data/<pkg>/shared_prefs/kindafax/<module>/config.toml`
+
+    :ivar location: Path to the config file.
+    :ivar dirs: PlatformDirs instance storing common filesystem paths.
     """
-    config_file: Final[str] = "config.toml"
-    config_file_path: Final[Path]
+    location: Final[Path]
     __config_dict: dict[str, Any] = dict()
 
     def __init__(self, module: str):
-        self.dirs = PlatformDirs(f"kindafax-{module}", "kindafax")
-        self.config_file_path = self.dirs.site_config_path.joinpath(
-            self.config_file
+        self.dirs = PlatformDirs(
+            APP_NAME,
+            APP_AUTHOR,
+            module,
+            ensure_exists=True
+        )
+
+        self.location = self.dirs.site_config_path.joinpath(
+            CONFIG_FILE
         )
 
     def __contains__(self, item) -> bool:
@@ -91,15 +103,14 @@ class Config:
         :raises ValueError: The config file was empty.
         :return:
         """
-        exists = self.config_file_path.exists()
-        is_file = self.config_file_path.is_file()
+        exists = self.location.exists()
+        is_file = self.location.is_file()
 
         if not (exists and is_file):
-            self.config_file_path.parent.mkdir(parents=True, exist_ok=True)
-            self.config_file_path.touch()
+            self.location.touch()
             raise ValueError("Config files cannot be empty.")
 
         config: dict
-        with open(self.config_file_path, "rb") as f:
+        with open(self.location, "rb") as f:
             config = tomllib.load(f)
         self.set(**config)
